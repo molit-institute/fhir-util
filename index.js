60bfefcdcf1e5abaf69a8eea72b5fc509e80e6f2
Jan Ross <jan.ross@molit.eu>
2019-10-16 16:06:51 +0200

    Added name-switch-option, removed identifier methods and tests

--- index.js
+++ index.js
@@ -8,11 +8,39 @@
  * fhirUtil.getStringFromHumanName(patient.name);
  *
  * @param {Object[]} humanName - the HumanNames Array
+ * @param {Boolean} switchNameOrder
  * @return {String} the human name
  */
-export function getStringFromHumanName(humanName) {
+export function getStringFromHumanName(humanName, switchNameOrder) {
   if (!humanName || !humanName[0]) {
     return "";
+  }
+
+  let givenFamilyName = "";
+  humanName.forEach((name, index, array) => {
+    if (switchNameOrder) {
+      if (name.family) {
+        givenFamilyName += name.family + " ";
+      }
+      if (name.given) {
+        givenFamilyName += name.given.join(" ");
+      }
+    } else {
+      if (name.given) {
+        givenFamilyName += name.given.join(" ");
+      }
+      if (name.family) {
+        givenFamilyName += " " + name.family;
+      }
+    }
+
+    if (index < array.length - 1) {
+      givenFamilyName += ", ";
+    }
+  });
+
+  if (givenFamilyName && givenFamilyName !== "") {
+    return givenFamilyName;
   }
 
   let textName = humanName
@@ -22,76 +50,7 @@ export function getStringFromHumanName(humanName) {
 
   if (textName && textName !== "") {
     return textName;
+  } else {
+    return "";
   }
-
-  let givenFamilyName = "";
-
-  humanName.forEach((name, index, array) => {
-    if (name.given) {
-      givenFamilyName += name.given.join(" ");
-    }
-    if (name.family) {
-      givenFamilyName += " " + name.family;
-    }
-    if (index < array.length - 1) {
-      givenFamilyName += ", ";
-    }
-  });
-
-  return givenFamilyName;
-}
-
-/**
- * Returns the first identifier with the given system. If you just want to get the identifier value, use {@link getIdentifierValueBySystem}. Returns null if any of the parameters is null or undefined.
- *
- * @param {Object[]} identifier - the identifier Array
- * @param {String} system - the system String
- * @return {Object} the identifier
- */
-export function getIdentifierBySystem(identifier, system) {
-  if (!identifier || !system) {
-    return null;
-  }
-  return identifier.find(identifier => identifier.system === system);
-}
-
-/**
- * Returns the value as String of an identifier String inthe format system|value. Returns null if any of the parameters is null or undefined.
- *
- * @param {String} identifierString
- */
-export function getIdentifierValueByIdentifierString(identifierString) {
-  if (!identifierString) {
-    return null;
-  }
-  return identifierString.substring(identifierString.indexOf("|") + 1);
-}
-
-/**
- * Returns the first identifier <b>value</b> it finds with the given system. If no identifier is found, null is returned. Returns "" if any of the parameters is null or undefined.
- *
- * @param {Array} identifiers
- * @param {String} system
- * @return {String|null} the identifier value
- */
-export function getIdentifierValueBySystem(identifiers, system) {
-  let identifier = getIdentifierBySystem(identifiers, system);
-  if (!identifier) {
-    return null;
-  }
-  return identifier.value;
-}
-
-/**
- * Returns the first identifier it finds with the given system. If no identifier is found, null is returned.
- *
- * @param {Object} resource - the FHIR resource
- * @param {String} system - the system of the identifier
- * @return {Object|null} the identifier
- */
-export function getIdentifierByResourceAndSystem(resource, system) {
-  if (!resource || !system) {
-    return null;
-  }
-  return getIdentifierBySystem(resource.identifier, system);
 }
--- test/fhir-util.spec.js
+++ test/fhir-util.spec.js
@@ -3,33 +3,8 @@ import * as fhirUtil from "../index";
 import humanNameText from "./fixtures/HumanName-Text.json";
 import humanNameNoText from "./fixtures/HumanName-NoText.json";
 import humanNameMultiple from "./fixtures/HumanName-Multiple.json";
-
-import identifier from "./fixtures/Identifier.json";
-
-const identifierResultObject = {
-  fhir_comments: ["MRN assigned by ACME healthcare on 6-May 2001"],
-  use: "usual",
-  type: {
-    coding: [
-      {
-        system: "http://hl7.org/fhir/v2/0203",
-        code: "MR"
-      }
-    ]
-  },
-  system: "urn:oid:1.2.36.146.595.217.0.1",
-  value: "12345",
-  period: {
-    start: "2001-05-06"
-  },
-  assigner: {
-    display: "Acme Healthcare"
-  }
-};
-
-const testResource = {
-  identifier: identifier
-};
+import humanNameNoGivenOrFamily from "./fixtures/HumanName-NoGivenOrFamily.json";
+import humanNameNothing from "./fixtures/humanName-Nothing.json";
 
 describe("FHIR Util", () => {
   describe("getStringFromHumanName(humanName)", () => {
@@ -37,72 +12,34 @@ describe("FHIR Util", () => {
       expect(fhirUtil.getStringFromHumanName()).toEqual("");
     });
     it("should return the value of the text field if present", () => {
-      expect(fhirUtil.getStringFromHumanName(humanNameText)).toEqual(
+      expect(fhirUtil.getStringFromHumanName(humanNameText, false)).toEqual(
         "Maxi Musterfrau"
       );
     });
     it("should return the value of given and family if no text is present", () => {
-      expect(fhirUtil.getStringFromHumanName(humanNameNoText)).toEqual(
+      expect(fhirUtil.getStringFromHumanName(humanNameNoText, false)).toEqual(
         "Peter James Chalmers"
       );
     });
-    it("should return a String with comma-separated names if multiple names are present", () => {
-      expect(fhirUtil.getStringFromHumanName(humanNameMultiple)).toEqual(
-        "Peter James Chalmers, Jim, Peter James Windsor"
+    it("should return the value of given and family if no text is present", () => {
+      expect(fhirUtil.getStringFromHumanName(humanNameNoText, true)).toEqual(
+        "Chalmers Peter James"
       );
     });
-  });
-
-  describe("getIdentifierBySystem(identifier, system)", () => {
-    it("should return the correct identifier", () => {
+    it("should return the value of text if no given or family is present", () => {
       expect(
-        fhirUtil.getIdentifierBySystem(
-          identifier,
-          "urn:oid:1.2.36.146.595.217.0.1"
-        )
-      ).toMatchObject(identifierResultObject);
+        fhirUtil.getStringFromHumanName(humanNameNoGivenOrFamily, false)
+      ).toEqual("Peter James Chalmers");
     });
-  });
-
-  describe("getIdentifierValueByIdentifierString(identifierString)", () => {
-    it("should return the correct value", () => {
-      expect(
-        fhirUtil.getIdentifierValueByIdentifierString(
-          "urn:oid:1.2.36.146.595.217.0.1|12345"
-        )
-      ).toEqual("12345");
+    it("should return '' if nothing is present", () => {
+      expect(fhirUtil.getStringFromHumanName(humanNameNothing, false)).toEqual(
+        ""
+      );
     });
-    it("should return null", () => {
-      expect(fhirUtil.getIdentifierValueByIdentifierString(null)).toBeNull;
-    });
-  });
-
-  describe("getIdentifierValueBySystem(identifiers, system)", () => {
-    it("should return the correct value", () => {
-      expect(
-        fhirUtil.getIdentifierValueBySystem(
-          identifier,
-          "urn:oid:1.2.36.146.595.217.0.1"
-        )
-      ).toEqual("12345");
-    });
-    it("should return null", () => {
-      expect(fhirUtil.getIdentifierValueBySystem(identifier, null)).toBeNull;
-    });
-  });
-
-  describe("getIdentifierByResourceAndSystem(resource, system)", () => {
-    it("should return the first correct identifier", () => {
-      expect(
-        fhirUtil.getIdentifierByResourceAndSystem(
-          testResource,
-          "urn:oid:1.2.36.146.595.217.0.1"
-        )
-      ).toMatchObject(identifierResultObject);
-    });
-    it("should return null", () => {
-      expect(fhirUtil.getIdentifierByResourceAndSystem(identifier, null))
-        .toBeNull;
+    it("should return a String with comma-separated names if multiple names are present", () => {
+      expect(fhirUtil.getStringFromHumanName(humanNameMultiple, false)).toEqual(
+        "Peter James Chalmers, Jim, Peter James Windsor"
+      );
     });
   });
 });
